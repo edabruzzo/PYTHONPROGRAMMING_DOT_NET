7b90674c83d88256b595436519defe76dd70bda8
edabruzzo <emmanuel.oliveira3@gmail.com>
2019-05-01 12:59:40 -0300

    xxxx

--- REGRESSION/regression.py
+++ REGRESSION/regression.py
@@ -1,7 +1,10 @@
 '''
 https://pythonprogramming.net/regression-introduction-machine-learning-tutorial/https://pythonprogramming.net/machine-learning-python-sklearn-intro
 
+COMMAND FOR EXECUTION: 
 
+    C:/Users/Emm/AppData/Local/Programs/Python/Python37-32/python.exe c/Users/Emm/Documents/NetBeansProjects/PYTHONPROGRAMMING_DOT_NET/REGRESSION
+/regression.py
 
 '''
 
@@ -19,10 +22,17 @@ df = df[['Adj. Open',  'Adj. High',  'Adj. Low',  'Adj. Close', 'Adj. Volume']]
 '''
 This creates a new column that is the % spread based on the closing price, which is our crude measure of volatility. Next, we'll do daily percent change:
 '''
-df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Close'] * 100.0
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Low'] * 100.0
+
+'''
+I went ahead and recorded the video version of this, not realizing my stake that it was high minus low divided by close. I meant to do High - Low, divided by the low. Feel free to fix that if you like.
+
+This creates a new column that is the % spread based on the closing price, which is our crude measure of volatility. Next, we'll do daily percent change:
+
+'''
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0;
 
 #Now we will define a new dataframe as:
-
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
 print(df.head());
